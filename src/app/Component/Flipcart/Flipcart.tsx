218fd0d2ce979090c880b9d0599b9f06f9c281a1
bhargav.info@gmail.com <bhargavpatel.info@gmail.com>
2024-07-18 17:11:19 +0530

    Your commit message here

--- src/app/Component/Flipcart/Flipcart.tsx
+++ src/app/Component/Flipcart/Flipcart.tsx
@@ -1,6 +1,6 @@
 
 import client from "@/config/contentful";
-
+import Image from 'next/image';
 const fetchHeader = async () => {
     const data = await client.getEntries({
         'content_type' : 'flipcart2'
@@ -16,11 +16,22 @@ export default async function Flipcart2( ) {
 
     return (
         <div>
-            {datatwo.map((datas, index) => (
+            { datatwo && datatwo.map((datas, index) => (
                 <div key={index} style={{ marginBottom: '10px', border: '1px solid #ccc', padding: '5px', width: '200px' }}>
            <div>Title: {datas.fields.title as string}</div>
 
-                    <img src={datas.fields.logo.fields.file.url} alt="" style={{ maxWidth: '100%', height: 'auto' }} />
+           <div>
+           {datas.fields.logo && (
+  <div>
+    <Image
+      src={(datas.fields.logo as any).fields.file.url}
+      alt=""
+      width={200} // Adjust width and height as per your design
+      height={150}
+    />
+  </div>
+)}
+</div>
                 </div>
             ))}
         </div>
